7cb39da2a1c2adaf76f838c8a183f6c798789246
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-03-15 01:29:35 +0530

    connection with mongo db atlas

--- server/app.js
+++ server/app.js
@@ -1,22 +1,35 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// middlewares : 
+// middlewares :
 // like jab tak user login nahi hua ,tab tak koi dusra page open nahi hoga type ka
 // interface banana hai toh middleware use karte hai,we have to mention middleware before the (req,res) method
 
-const middleware =(req,res,next)=>{
-    console.log("hello from middleware ");
-    //after the work is complete like authentication ,call next function 
-    next();
-}
+const middleware = (req, res, next) => {
+  console.log("hello from middleware ");
+  //after the work is complete like authentication ,call next function
+  next();
+};
 
-
+const DB =
+  "mongodb+srv://sushsk:Sushant%40123@cluster0.neapw.mongodb.net/mernstack?retryWrites=true&w=majority";
+mongoose
+  .connect(DB, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection established to mongo");
+  })
+  .catch((err) => {
+    console.log("no connection established to mongo");
+  });
 app.get("/", (req, res) => {
   res.send("hello world from server");
 });
-app.get("/about",middleware, (req, res) => {
+app.get("/about", middleware, (req, res) => {
   res.send("hello world from about");
 });
 app.get("/contact", (req, res) => {

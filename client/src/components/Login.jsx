aabec5ada21c60091a884b5a1acdaff4d9f276cd
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-03-23 00:31:48 +0530

    login jsx updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
-
+import {NavLink} from "react-router-dom" 
+import {MdPassword, MdEmail } from "react-icons/md";
 const Login = () => {
   return (
-    <div>Login</div>
+
+    <div>
+     
+     <section className="signup d-flex justify-content-center" >
+
+        <div className="row mt-3"> 
+             <div className="container mt-5"> 
+          <div class="form-outline d-none d-sm-block ">
+          <figure>
+          <iframe src="https://embed.lottiefiles.com/animation/71361"  frameBorder="0" height="200vhvh" width="200vw"style={{pointerEvents: "none",Decoration:"none"}} title="Animation"></iframe>
+          </figure>
+          <NavLink to="/signup" class=" mx-3 px-3 align-center text-center" >I am already registered</NavLink>
+          </div>
+          <form classname=" card" id="register-form">
+            <div className="form-title " style={{ fontSize: "29px" }}>
+              <b> Sign up</b>
+            </div>
+          
+           <div class="form-outline ">
+              <input placeholder="Email" name="email" type="email" id="email" class="form-control  px-4"  />
+              <label class="form-label" for="form6Example5">
+              <MdEmail/>  Email
+              </label>
+            </div>
+            <div class="form-outline ">
+              <input placeholder="password" name="password" type="password" id="password" class="form-control m-2 px-4"  />
+              <label class="form-label" for="form6Example4">
+              <MdPassword/>  password
+              </label>
+            </div>
+            <div className = " form-group form-button " >
+<input type= "submit" name="signin" id="signin" className = "btn-primary form-submit" value="log in"/>
+</div>
+          </form>
+
+        </div>
+      </div>
+    </section>
+
+    </div>
+
   )
 }
 
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -9,14 +9,14 @@ const Signup = () => {
     <section className="signup d-flex justify-content-center" >
       <div className="container mt-5">
         <div className="row mt-3">
-          <form classname="register-form" id="register-form">
+          <form classname="register-form card" id="register-form">
             <div className="form-title " style={{ fontSize: "29px" }}>
               <b> Sign up</b>
             </div>
             <div class="row ">
               <div class="col">
                 <div class="form-outline">
-                  <input placeholder="Name" name="name" type="text" id="name" class="form-control" />
+                  <input placeholder="Name" name="name" type="text" id="name" class="form-control  px-4"  />
                   <label class="form-label" for="form6Example1">
                    <FaUserAlt/>  name
                   </label>
@@ -25,47 +25,46 @@ const Signup = () => {
    
             </div>
            <div class="form-outline ">
-              <input placeholder="Name" name="email" type="email" id="email" class="form-control" />
+              <input placeholder="Email" name="email" type="email" id="email" class="form-control  px-4"  />
               <label class="form-label" for="form6Example5">
               <MdEmail/>  Email
               </label>
             </div>
             
               <div class="form-outline ">
-              <input placeholder="phone" name="phone" type="number" id="phone" class="form-control" />
+              <input placeholder="phone" name="phone" type="number" id="phone" class="form-control  px-4"  />
               <label class="form-label" for="form6Example6">
               <BsFillPhoneVibrateFill/>  Phone
               </label>
             </div>
             <div class="form-outline ">
-              <input placeholder="work" name="work" type="text" id="work" class="form-control" />
+              <input placeholder="work" name="work" type="text" id="work" class="form-control  px-4"  />
               <label class="form-label" for="form6Example3">
               <FcWorkflow/>  Work
               </label>
             </div>
 
             <div class="form-outline ">
-              <input placeholder="password" name="password" type="password" id="password" class="form-control" />
+              <input placeholder="password" name="password" type="password" id="password" class="form-control m-2 px-4"  />
               <label class="form-label" for="form6Example4">
               <MdPassword/>  password
               </label>
             </div>
             <div class="form-outline ">
-              <input placeholder="cpassword" name="cpassword" type="password" id="cpassword" class="form-control" />
+              <input placeholder="cpassword" name="cpassword" type="password" id="cpassword" class="form-control  px-4"  />
               <label class="form-label" for="form6Example4">
               <MdPassword/>  cpassword
               </label>
             </div>
-
-            <button type="submit" class="btn btn-primary btn-block ">
-              Submit
-            </button>
+            <div className = " form-group form-button " >
+<input type= "submit" name="signin" id="signin" className = "btn-primary form-submit" value="register"/>
+</div>
           </form>
           <div class="form-outline d-none d-sm-block ">
           <figure>
-          <iframe src="https://embed.lottiefiles.com/animation/78126"  frameBorder="0" height="500vh" width="500vw"style={{pointerEvents: "none",Decoration:"none"}} title="Animation"></iframe>
+          <iframe src="https://embed.lottiefiles.com/animation/78126"  frameBorder="0" height="480vh" width="500vw"style={{pointerEvents: "none",Decoration:"none"}} title="Animation"></iframe>
           </figure>
-          <NavLink to="/login" class="signup_link" >I am already registered</NavLink>
+          <NavLink to="/login" class=" mx-3 px-3 align-center text-center" >I am already registered</NavLink>
           </div>
         </div>
       </div>

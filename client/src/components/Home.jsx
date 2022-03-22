d8485eae6e08975325298b6920cb58ff9cd1d09d
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-03-22 22:02:18 +0530

    added navbar and react  router

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+        <>
+    <h3 className="text-center pt-5" >WELCOME</h3>
+    <h1 className="text-center"> we are Mern developer </h1>
+  </>)
+}
+
+export default Home
